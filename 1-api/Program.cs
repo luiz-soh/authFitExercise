d94305cd3e3591459e1e2d6c0c987af4f061a9cd
luiz-soh <souza_luiz@live.com>
2023-04-11 18:12:45 -0300

    atualizando pacotes

--- 1-api/Program.cs
+++ 1-api/Program.cs
@@ -93,11 +93,8 @@ app.UseCors("corsapp");
 //app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
